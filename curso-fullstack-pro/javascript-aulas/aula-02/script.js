acedef3fd2c9c3203688495f50387fe577b12de3
Guilherme Pereira <guiipereiraarts@gmail.com>
2026-02-02 15:08:36 -0300

    Funções com JavaScript

--- curso-fullstack-pro/javascript-aulas/aula-02/script.js
+++ curso-fullstack-pro/javascript-aulas/aula-02/script.js
@@ -20,3 +20,18 @@ function sair() {
     alert("Até mais!");
     area.innerHTML = "Você saiu!";
 }
+
+function mediaAluno(nota1, nota2) {
+    let media = (nota1 + nota2) / 2;
+
+    if (media >= 7) {
+        console.log(`Aluno aprovado com a média: ${media}`);
+    } else {
+        console.log(`Aluno reprovado com a média: ${media}`);
+    }
+}
+
+function aluno(nome, curso) {
+    let mensagem = `Olá ${nome} seja bem vindo ao curso de ${curso}`;
+    console.log(mensagem);
+}
