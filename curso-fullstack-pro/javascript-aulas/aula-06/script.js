aaaf864755c0f2b38ceb80d4e27dc7a8fbd04595
Guilherme Pereira <guiipereiraarts@gmail.com>
2026-02-10 10:56:32 -0300

    Exercício finalizado calculadora IMC

--- curso-fullstack-pro/javascript-aulas/aula-06/script.js
+++ curso-fullstack-pro/javascript-aulas/aula-06/script.js
@@ -6,3 +6,36 @@ Entre 25 e 29,99 Acima do peso
 */
 
 // Peso / (altura*altura);
+
+let peso;
+let altura;
+let imc;
+let resultado;
+
+function calcular(event) {
+    event.preventDefault();
+
+    peso =  document.getElementById('peso').value;
+    altura = document.getElementById('altura').value;
+
+    imc = peso / (altura * altura);
+    resultado = document.getElementById('resultado');
+
+    if (imc < 17) {
+        resultado.innerHTML = `<br> Seu resultado foi ${imc.toFixed(2)} <br> Cuidado você está muito abaixo do peso!`;
+    }else if (imc > 17 && imc <= 18.49) {
+        resultado.innerHTML = `<br> Seu resultado foi ${imc.toFixed(2)} <br> Você está abaixo do peso!`;
+
+    }else if (imc >= 18.5 && imc < 24.99) {
+        resultado.innerHTML = `<br> Seu resultado foi ${imc.toFixed(2)} <br> Você está no peso ideal!`;
+
+    }else if (imc > 25 && imc <= 29.99 ) {
+        resultado.innerHTML = `<br> Seu resultado foi ${imc.toFixed(2)} <br> Você está acima do peso!`;
+
+    }else if (imc >= 30) {
+        resultado.innerHTML = `<br> Seu resultado foi ${imc.toFixed(2)} <br> Cuidado você está muito acima do peso!`;
+    }
+
+    document.getElementById('peso').value = ``;
+    document.getElementById('altura').value = ``;
+}
