b34123eab564f9bda6549970fbf44cfda2237662
Guilherme Pereira <guiipereiraarts@gmail.com>
2026-02-12 10:28:56 -0300

    Temporizadores em JavaScript

---
+++ curso-fullstack-pro/javascript-aulas/aula-07/script.js
@@ -0,0 +1,13 @@
+// setInterval
+
+const acao = () => {
+    console.log("Executando...")
+}
+
+// let timer = setInterval(() => {
+//     document.writeln("Executando! <br/>");
+// }, 1000);
+
+setTimeout(() => {
+    document.writeln("Executando! <br/>");
+}, 3000);
