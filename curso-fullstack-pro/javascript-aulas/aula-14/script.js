1586e37a71fe62262ddb64b6d0725ce51e288729
Guilherme Pereira <guiipereiraarts@gmail.com>
2026-04-02 16:12:14 -0300

    Método includes() em JavaScript

---
+++ curso-fullstack-pro/javascript-aulas/aula-14/script.js
@@ -0,0 +1,27 @@
+
+// Entendendo os Métodos: includes, startswith, endswith.
+/*
+includes = inclui
+startswith = começa com
+endswith = termina com
+*/
+
+
+/* INCLUDES()
+let nomes = ["Guilherme", "Sofia", "Maria", "Gustavo"];
+
+console.log(nomes.includes("Bella"));
+
+if (nomes.includes("Guilherme")) {
+    console.log("ESTA NA LISTA");
+} else {
+    console.log("NÃO ESTA NA LISTA");
+}
+*/
+
+// STARTSWITH() & ENDSWITH()
+
+let nome = "Guilherme";
+
+console.log(nome.startsWith("G"));
+console.log(nome.endsWith("me"));
