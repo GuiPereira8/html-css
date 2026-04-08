7974c5b2adb5268a2a627d75b16a72d40878470d
Guilherme Pereira <guiipereiraarts@gmail.com>
2026-04-08 16:02:02 -0300

    Desafio Mega Sena
    Resultado do desafio mega sena do curso, resultado final para ser comparado com a resposta feita em "resp-mega-sena".

---
+++ curso-fullstack-pro/desafios/mega-sena/script.js
@@ -0,0 +1,18 @@
+function gerarNumeroMega(qtdNumeros) {
+    
+    if (qtdNumeros < 6 || qtdNumeros > 9) {
+        console.log("Apenas números de 6 até 9");
+        return [];
+    }
+
+    const numeros = [];
+    while (numeros.length < qtdNumeros) {
+        const numeroAleatorio = Math.floor(Math.random() * 60) +1;
+        if (!numeros.includes(numeroAleatorio)) {
+            numeros.push(numeroAleatorio);
+        }
+    }
+    return numeros;
+}
+const numeroSorteado = gerarNumeroMega(6);
+console.log(numeroSorteado);
